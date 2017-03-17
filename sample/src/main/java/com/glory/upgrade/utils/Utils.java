5b16669f0d5d1dbc971e6b7880fd7a9ea3f9e3c9
liuzhenrong <liu.zhenrong@jyall.com>
2017-03-17 17:38:11 +0800

    优化

--- sample/src/main/java/com/glory/upgrade/utils/Utils.java
+++ sample/src/main/java/com/glory/upgrade/utils/Utils.java
@@ -15,15 +15,20 @@ public class Utils {
      */
     public static void checkVersion(final Activity activity, final boolean isShowToast) {
         new ApkUpgradeTool.Builder(activity)
+                //apk下载地址
                 .apkUrl("http://www.lianjia.com/client/download?ua=android&channel=homelink")
+                //版本号
                 .versionCode(2)
+                //升级回调用了初始化dialog
                 .onUpdateListener(new OnUpgradeListener() {
                     @Override
                     public void initDialog(ApkUpgradeTool.Builder builder) {
                         DialogUtils.showUpdateDialog(activity, builder);
                     }
 
-                }).build().updateVersion(isShowToast);
+                }).build()
+                //是否显示toast提示
+                .updateVersion(isShowToast);
 
     }
 }
